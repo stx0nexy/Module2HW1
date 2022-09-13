27b88e413443cb8f2775cefedfb74f1fb06e1837
Nesterenko Sophia <nykolay76@yandex.ru>
2022-09-13 14:42:13 +0200

    add logger

---
+++ Module2HW1/Actions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Module2HW1
+{
+	public static class Actions
+	{
+		private static readonly Logger logger = Logger.GetLogger();
+
+		public static Result StartMet()
+		{
+			logger.AddLog(new Log(DateTime.Now, LogType.Info, $"Start method: {nameof(StartMet)}"));
+			return new Result(true, "Start method completed.");
+		}
+
+		public static Result SkipMet()
+		{
+			logger.AddLog(new Log(DateTime.Now, LogType.Warning, $"Skipped logic in method: {nameof(SkipMet)}"));
+			return new Result(true, "Skipped logic in method");
+		}
+
+		public static Result BrokeMet()
+		{
+			logger.AddLog(new Log(DateTime.Now, LogType.Error, $"Broke method: {nameof(BrokeMet)}"));
+			return new Result(false, "I broke a logic");
+		}
+	}
+}
---
+++ Module2HW1/Result.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace Module2HW1
+{
+	public class Result
+	{
+		private readonly bool status;
+		private readonly string message;
+
+		public bool Status
+		{
+			get => status;
+		}
+
+		public string Message
+		{
+			get => message;
+		}
+
+		public Result(bool status, string message)
+		{
+			this.status = status;
+			this.message = message;
+		}
+	}
+}
